21c904e5228dce3ded258a0b6233832400a995e4
VioletViolaVi <vivian.v18723@gmail.com>
2020-09-14 18:10:22 +0000

    Complete homepage header and css.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home'
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
