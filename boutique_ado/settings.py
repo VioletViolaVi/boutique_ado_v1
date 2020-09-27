079faf4e12ec507e38e2d65b50a12d87da4f4e87
VioletViolaVi <vivian.v18723@gmail.com>
2020-09-27 22:16:47 +0000

    059 - deployment part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,6 +178,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if "USE_AWS" in os.environ:
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = "boutique-ado-viv"
     AWS_S3_REGION_NAME = "us-east-1"
