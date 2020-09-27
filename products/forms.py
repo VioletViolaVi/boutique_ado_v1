8e04357fa762e298c0a7306930c26e9ef7bb1b2c
VioletViolaVi <vivian.v18723@gmail.com>
2020-09-27 16:30:39 +0000

    Product admin - Fix the image field part 1.

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,15 @@
-from django import forms
 from .models import Product, Category
+from django import forms
+from.widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
+
+    image = forms.ImageField(label="Image", required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
