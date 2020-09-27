c3a2f204f2eff09456251d6f6dda8dfcc716c7b8
VioletViolaVi <vivian.v18723@gmail.com>
2020-09-27 22:14:13 +0000

    059 - deployment part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -132,7 +133,7 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
- 
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -175,6 +176,25 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if "USE_AWS" in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "boutique-ado-viv"
+    AWS_S3_REGION_NAME = "us-east-1"
+    AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media files
+    STATICFILES_STORAGE = "custom_storage.StaticStorage"
+    STATICFILES_LOCATION = "static"
+    DEFAULT_FILES_STORAGE = "custom_storages.MdeiaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # Override static and mdeia URLs in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
